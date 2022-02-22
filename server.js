92b007c3329b59e0290d0737c7e4b0aad8859f0e
FamutimiEmmanuel <olamide.famutimi@gmail.com>
2022-02-21 22:32:23 -0800

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,12 @@ app.use(require('./routes/users'))
 app.use(require('./routes/contacts'))
 app.use(require('./routes/auth'))
 
+// load static files
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
